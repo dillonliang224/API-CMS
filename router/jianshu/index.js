b5493e2c482b8bd7589e1502cbd4ff8712735cb3
dillon <liangf@jkinvest.cn>
2017-07-14 17:28:19 +0800

    no message

--- router/jianshu/index.js
+++ router/jianshu/index.js
@@ -5,5 +5,5 @@
 const article = require('../../controller/jianshu/article');
 
 exports.map = function (app) {
-    app.get('/articles', article.getArticleList);         // 获取简书文章列表
+    app.get('/articles', article.getArticles);         // 获取简书文章列表
 };
