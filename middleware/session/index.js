ab98bce6ee1e30a35e83ba20c338f593d1e4a7b5
dillon <liangf@jkinvest.cn>
2017-08-07 14:59:29 +0800

    no message

--- middleware/session/index.js
+++ middleware/session/index.js
@@ -12,49 +12,6 @@ const NODE_ENV = process.env.NODE_ENV;
  * */
 module.exports = function () {
     return function (req, res, next) {
-
-        req.session = {};
-        var parseCookie = function(cookie){
-            var cookies = {};
-            if (!cookie) return cookies;
-            var list = cookie.split(";");
-            for (var i = 0; i < list.length; i++) {
-                var pair = list[i].split("=");
-                cookies[pair[0].trim()] = pair[1];
-            }
-            return cookies;
-        };
-
-        let cookies = parseCookie(req.headers.cookie);
-        // if (!cookies || !cookies.token){
-        //     return res.json({
-        //         flag: '0401',
-        //         msg:  '',
-        //         result: {
-        //             a:req.headers.authorization
-        //         }
-        //     })
-        // }
-
-        let token = null;
-        let now = Date.now();
-
-        if(req.method === 'GET' || req.method === 'DELETE'){
-            token = req.query.login_token;
-        }else{
-            token = req.body.login_token;
-        }
-        
-        token = token ? token.trim() : null;
-
-        if(!token && (NODE_ENV === 'pre' || NODE_ENV === 'dev')){
-            req.session = {
-                id: '58aa50177ddbf5507c51f082',
-                username: 'synder',
-                expire: Date.now() + 100000000
-            };
-        }
-
         return next();
     }
 };
